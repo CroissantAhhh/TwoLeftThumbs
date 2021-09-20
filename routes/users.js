24eb96728eee088ba7b4d079096569b396b72a26
CroissantAhhh <jasonz4@uw.edu>
2021-09-20 14:03:22 -0700

    adding user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/user/register', csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render('user-register', {
+    title: 'Register',
+    user,
+    csrfToken: req.csrfToken(),
+  });
 });
+
+const userValidators = [
+  // TODO Define the user validators.
+];
+
+router.post('/user/register', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      emailAddress,
+      userName,
+      password,
+    } = req.body;
+
+    const user = db.User.build({
+      emailAddress,
+      userName,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      await user.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render('user-register', {
+        title: 'Register',
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  }));
 
 module.exports = router;
