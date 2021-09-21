137bfa5d66b2b1c5e48cf04cdcbcbedfdb1039e8
CroissantAhhh <jasonz4@uw.edu>
2021-09-21 13:59:52 -0700

    user answer seeding

--- db/seeders/20210921155235-Users.js
+++ db/seeders/20210921155235-Users.js
@@ -6,6 +6,10 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {userName: 'Michael Scott', email: 'mscott@gmail.com', hashedPassword: bcrypt.hashSync('P@ssw0rd', 10), createdAt: new Date(), updatedAt: new Date()},
       {userName: 'Kevin Malone', email: 'kmalone@gmail.com', hashedPassword: bcrypt.hashSync('P@ssw0rd', 10), createdAt: new Date(), updatedAt: new Date()},
+      {userName: 'Jim Halpert', email: 'jhalpert@gmail.com', hashedPassword: bcrypt.hashSync('Pam2!', 10), createdAt: new Date(), updatedAt: new Date()},
+      {userName: 'Pam Beesly', email: 'pbeesly@gmail.com', hashedPassword: bcrypt.hashSync('artSchool1!', 10), createdAt: new Date(), updatedAt: new Date()},
+      {userName: 'Dwight Schrute', email: 'dschrute@gmail.com', hashedPassword: bcrypt.hashSync('beetsBears2@', 10), createdAt: new Date(), updatedAt: new Date()},
+      {userName: 'Andy Bernard', email: 'abernard@gmail.com', hashedPassword: bcrypt.hashSync('Cornell9#', 10), createdAt: new Date(), updatedAt: new Date()},
     ], {});
   },
 
