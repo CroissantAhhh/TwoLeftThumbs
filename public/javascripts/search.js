fbb995b82ddf36df15140323469d85da7f8219c7
Victor Hou <hou_v@yahoo.com>
2021-09-23 15:18:06 -0700

    Implemented searching with enter/return key

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,14 +3,12 @@ const searchButton = document.getElementById("searchButton");
 
 window.addEventListener("DOMContentLoaded", (e) => {
 	searchButton.addEventListener("click", async (e) => {
-		console.log(searchValue.value.length);
 		let term = searchValue.value.split(" ").join("+");
 		window.location.href = `/search?q=${term}`;
 	});
 
-	searchButton.addEventListener("keypress", async (e) => {
-		if (e.key === "Return") {
-			console.log(searchValue.value.length);
+	searchValue.addEventListener("keypress", async (e) => {
+		if (e.key === "Enter") {
 			let term = searchValue.value.split(" ").join("+");
 			window.location.href = `/search?q=${term}`;
 		}
