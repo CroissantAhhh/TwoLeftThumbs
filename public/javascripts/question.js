47139264a39ca0c85cc77824ca7a0abbbc2a72b5
aftonslone <afton.slone@gmail.com>
2021-09-26 02:28:20 -0400

    now able to delete an answer

--- public/javascripts/question.js
+++ public/javascripts/question.js
@@ -2,6 +2,14 @@ const answerUpVote = document.querySelectorAll(".answer_upVote");
 const answerDownVote = document.querySelectorAll(".answer_downVote");
 const questionUpVote = document.querySelector(".question_upVote");
 const questionDownVote = document.querySelector(".question_downVote");
+const questionDelete = document.querySelector(".question_delete");
+const questionEdit = document.querySelector(".question_edit");
+const answerDelete = document.querySelectorAll(".answer_delete");
+const answerEdit = document.querySelectorAll(".answer_edit");
+const form = document.querySelector(".add_answer");
+
+const formData = new FormData(form);
+const _csrf = formData.get("_csrf");
 
 answerUpVote.forEach((answer) => {
   answer.addEventListener("click", async (e) => {
@@ -53,15 +61,33 @@ questionUpVote.addEventListener("click", async (e) => {
 });
 
 questionDownVote.addEventListener("click", async (e) => {
-    const id = e.target.dataset.id;
-    const body = { id, dir: -1 };
-    const res = await fetch(`/votes/question/${id}`, {
-      method: "POST",
+  const id = e.target.dataset.id;
+  const body = { id, dir: -1 };
+  const res = await fetch(`/votes/question/${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+  const questionId = questionUpVote.dataset.id;
+  window.location.href = `/questions/${questionId}`;
+});
+
+answerDelete.forEach((answer) => {
+  answer.addEventListener("click", async (e) => {
+    id = e.target.dataset.id;
+    const questionId = questionUpVote.dataset.id;
+    const body = { _csrf };
+    let res = await fetch(`/answers/${id}`, {
+      method: "DELETE",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(body),
     });
-    const questionId = questionUpVote.dataset.id;
-    window.location.href = `/questions/${questionId}`;
+    if (res.status === 200) {
+      window.location.href = `/questions/${questionId}`;
+    }
   });
+});
