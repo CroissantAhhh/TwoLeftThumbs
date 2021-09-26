735b52fd0cb31aae3ef57369b83d9fec64055dcc
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-09-26 15:46:25 -0700

    Created team route, team page, and team css

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/", (req, res) => {
-    res.render('about')
-})
-
+	res.render("about");
+});
 
 module.exports = router;
