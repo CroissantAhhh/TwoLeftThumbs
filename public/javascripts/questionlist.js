6f7693013bd7898e23bb68bd191316f9a6f0a118
Adam Guan <86337369+AdamHGuan@users.noreply.github.com>
2021-09-26 14:12:46 -0700

    Update new btn on Question not found page

--- public/javascripts/questionlist.js
+++ public/javascripts/questionlist.js
@@ -1,5 +1,10 @@
-const askQuestionBtn = document.querySelector(".askQuestion_btn")
+const askQuestionBtn = document.querySelector(".askQuestion_btn");
+const backToQuestionsBtn = document.querySelector(".backToQuestions_btn");
 
 askQuestionBtn.addEventListener("click", async (e) => {
-    window.location.href = '/questions/add'
-})
+	window.location.href = "/questions/add";
+});
+
+backToQuestionsBtn.addEventListener("click", async (e) => {
+	window.location.href = "/questions";
+});
